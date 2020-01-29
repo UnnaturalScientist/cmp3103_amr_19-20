e7c8c350671614fe991ce588bf76c507548f858d
PEI07088957 <42605157+UnnaturalScientist@users.noreply.github.com>
2020-01-29 09:37:14 +0000

    Create ws2_task2.py

---
+++ ws2_task2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from geometry_msgs.msg import Twist # This is the message type the robot uses for velocities
+
+
+class CommandVelocity():
+    """Driving my robot
+    """
+
+    def __init__(self):
+        rospy.loginfo("Starting node")
+        self.pub = rospy.Publisher("/cmd_vel", Twist) # Creating a publisher with whatever name...
+
+    # A function to send velocities until the node is killed
+    def send_velocities(self):
+        r = rospy.Rate(10) # Setting a rate (hz) at which to publish
+        while not rospy.is_shutdown(): # Runnin until killed
+            rospy.loginfo("Sending commands")
+            twist_msg = Twist() # Creating a new message to send to the robot
+
+            twist_msg.angular.z = 0.5 -r 1 # I see no reason why this should not work...
+
+            # rotate on the spot or drive in a circle?
+
+            self.pub.publish(twist_msg) # Sending the message via our publisher
+            r.sleep() # Calling sleep to ensure the rate we set above
+
+if __name__ == '__main__':
+    rospy.init_node("command_velocity")
+    cv = CommandVelocity()
+    cv.send_velocities() # Calling the function
+    rospy.spin()
